43093f8e608448265cf75bf5ced11df5e6821d75
Jay <msg.jay@gmail.com>
2020-01-03 22:13:43 -0500

    added functionality to read a list of items

--- scraper.py
+++ scraper.py
@@ -6,24 +6,34 @@ from email.mime.text import MIMEText
 import requests
 from bs4 import BeautifulSoup
 
+# setting.py contains the server, port, and email info for the sending and receiving emails
 from settings import from_password, from_port, from_server, from_user, to_email
 
 
 def import_price_list():
+    """
+    Reads the items_list.json file and imports a list of items to price check
+    """
     with open("item_list.json", "r") as f:
         item_list = json.load(f)
 
     return item_list
 
 
-def check_price():
-    # URL to be tracked
-    URL = "https://www.amazon.ca/Moneysworth-Best-Shoe-Cream-Black/dp/B00BVZ5ZR6/ref=sr_1_2?dchild=1&keywords=Moneysworth+and+Best+Shoe+Cream&qid=1578044020&s=sports&sr=1-2"
+def save_price_list(item_list):
+    """
+    Saves the item_list to json file
+    """
+    with open("item_list.json", "w") as fout:
+        json.dump(item_list, fout)
 
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
-    }
 
+def check_price(URL, headers):
+    """
+    Scrapes Amazon.ca and obtiains the price of the item. If the prices is below a target prices
+    (obtained from items_list.json), then it will send and email with price alert.
+    Returns: price of the item
+    """
     page = requests.get(URL, headers=headers)
 
     # Amazon makes HTML code with javascript, so you can trick it using 2 soups
@@ -36,20 +46,23 @@ def check_price():
     # Price can contain currency symbols and "CAD"
     price = float(re.findall(r"\d+.\d+", price)[0])
 
-    # If Prices less that target price, send email
-    if price < 10.00:
-        send_email(title, price, URL)
+    return (
+        price,
+        title,
+    )
 
 
 def send_email(title, price, URL):
-
+    """
+    Helper function to send email with the price alert details.
+    Requires the server, port, user email and password for the sending email.
+    """
     message = MIMEText(f"Price dropped to {price}. \nCheck amazon link: {URL}")
     message["Subject"] = f"Price dropped: {title}!"
     message["From"] = from_user
     message["To"] = to_email
 
     # print(message.as_string())
-
     try:
         server = smtplib.SMTP(from_server, from_port)
         server.connect(from_server, from_port)
@@ -59,13 +72,41 @@ def send_email(title, price, URL):
         server.sendmail(from_user, to_email, message.as_string())
         server.quit()
         print("Email sent successfully")
-    except:
+    except Exception as e:
         print("Can't send email")
+        print(e)
 
 
 if __name__ == "__main__":
-    check_price()
-    # item_list = import_price_list()
-    # print(item_list)
-    # print(type(item_list))
-    # print(item_list.keys())
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+    }
+
+    # Import list of items to check
+    item_list = import_price_list()
+
+    for index, item in enumerate(item_list):
+
+        item_name = item["item_name"]
+        item_url = item["item_url"]
+        target_price = item["target_price"]
+        last_price = item["last_price"]
+
+        # Check latest price from Amazon
+        print(f"Checking {item_name}")
+        item_price, item_title = check_price(item_url, headers)
+        print(f"{item_title}, {item_price}")
+
+        # If current price is less that target price: send email
+        if item_price < target_price:
+            send_email(item_title, item_price, item_url)
+
+        # If the item has dropped in the price since the last check, update "last_price"
+        if item_price < last_price:
+            item_list[index]["last_price"] = item_price
+
+        # Update the changes to JSON file
+        save_price_list(item_list)
+
+    print(item_list)
